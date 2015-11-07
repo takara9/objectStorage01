23633b8078104162de3f85d2189893f9f8ba0aac
高良真穂 <maho@imac.local>
2015-11-07 19:55:25 +0900

    edit metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'objectStorage01'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Maho Takara'
+maintainer_email 'takara9@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures objectStorage01'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
